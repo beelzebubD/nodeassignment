d76228f6b2131894b15d69852c6777588f3bf2f3
Divyanshu Singh <sng.divyanshu@gmail.com>
2018-04-13 23:32:36 +0530

    working

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var http = require('http');
 var app = require('./app');
 var mysql = require('mysql');
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "9044"
+  host: "tuy8t6uuvh43khkk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "kdlfflhaz60phgvg",
+  password: "irgbstk2n1jq8lpg"
 });
 con.connect(function(err) {
   if (err) throw err;
@@ -14,7 +14,7 @@ con.connect(function(err) {
     console.log("Database mydb");
   });
 });
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3306;
 http.createServer(app.handleRequest).listen(port,function(){
 	console.log("APP is running on port"+port);
 });
