16c16f22b75d678104a61f31ce98fafdafd38a46
Divyanshu Singh <sng.divyanshu@gmail.com>
2018-04-13 21:21:17 +0530

    working

--- app.js
+++ app.js
@@ -164,6 +164,9 @@ module.exports = {
 
       var path = url.parse(request.url).pathname;
       switch (path) {
+          case '/':
+              renderHTML('./database.html', response);
+              break;
           case '/createusername':
               extract(request, response , "create");
               break;
